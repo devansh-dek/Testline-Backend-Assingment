4cd180d6a01f62a0082c80d93ff6b40683cedfe8
Devansh Khandelwal <devanshksg@gmail.com>
2025-01-30 23:01:51 +0530

    feat: Add basic server setup and interfaces

---
+++ src/interfaces/quiz.interface.ts
@@ -0,0 +1,32 @@
+export interface Quiz {
+    id: number;
+    name: string | null;
+    title: string;
+    description: string;
+    difficulty_level: string | null;
+    topic: string;
+    duration: number;
+    negative_marks: string;
+    correct_answer_marks: string;
+    questions_count: number;
+    max_mistake_count: number;
+  }
+  
+  export interface QuizSubmission {
+    id: number;
+    quiz_id: number;
+    user_id: string;
+    score: number;
+    trophy_level: number;
+    accuracy: string;
+    speed: string;
+    final_score: string;
+    negative_score: string;
+    correct_answers: number;
+    incorrect_answers: number;
+    mistakes_corrected: number;
+    initial_mistake_count: number;
+    response_map: Record<string, number>;
+    quiz: Quiz;
+  }
+  
